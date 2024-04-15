6ff313ee053e221ed142d9c8a4eaf24aa2b2e731
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-15 18:31:07 +0100

    Add 'role' field to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
