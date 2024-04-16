2b9f355f51e208ca60aa581f0450ab26c885d610
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-16 16:23:03 +0100

    Add Blog and Category models

---
+++ app/Models/Blog.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image_url',
+        'user_id',
+        'category_id',
+        'likes_count',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() 
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
+?>
